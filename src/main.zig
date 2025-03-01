cc026040ffe232592fa1458c5f93af460d23e5d3
ParkerHitch <parker.hitchcock@gmail.com>
2025-02-28 23:30:38 -0500

    feat: Add struct for SR chain with DMA-based writes

--- src/main.zig
+++ src/main.zig
@@ -2,34 +2,69 @@ const std = @import("std");
 const microzig = @import("microzig");
 const peripherals = microzig.chip.peripherals;
 const RCC = microzig.chip.peripherals.RCC;
+const LedMatrix = @import("subsystems/matrix.zig");
+const TestSR = LedMatrix.SrChain(2, 0b111);
 
 const ChipInit = @import("init/general.zig");
 
-extern fn enable_dma() void;
-extern fn get_peen(u8) u8;
+const DebouncedBtn = struct {
+    history: u8 = 0,
+    state: u1 = 0,
+
+    pub fn update(self: *@This(), val: u1) void {
+        self.history = (self.history << 1) | val;
+        if (self.history == 0xFF) {
+            self.state = 1;
+        } else if (self.history == 0x00) {
+            self.state = 0;
+        }
+    }
+};
 
 pub fn main() void {
     ChipInit.internal_clock();
-    enable_dma();
-    const p: u8 = get_peen(0);
-    if (p != 'p') {
-        return;
-    }
+
+    TestSR.setup();
+
+    // const p: u8 = get_peen(0);
+    // if (p != 'p') {
+    //     return;
+    // }
 
     RCC.AHBENR.modify(.{
+        .GPIOBEN = 1,
         .GPIOCEN = 1,
     });
 
     peripherals.GPIOC.MODER.modify(.{
         .@"MODER[6]" = .Output,
     });
+    peripherals.GPIOB.MODER.modify(.{
+        .@"MODER[2]" = .Input,
+    });
+    peripherals.GPIOB.PUPDR.modify(.{
+        .@"PUPDR[2]" = .PullDown,
+    });
+
+    var SW3 = DebouncedBtn{};
+    var last_state: u1 = 0;
+    var id: u3 = 0;
 
     while (true) {
-        for (0..2_000_000) |_| {
+        for (0..10_000) |_| {
             asm volatile ("nop");
         }
-        var val = peripherals.GPIOC.ODR.read();
-        val.@"ODR[6]" = if (val.@"ODR[6]" == .High) .Low else .High;
-        peripherals.GPIOC.ODR.write(val);
+        SW3.update(@intFromEnum(peripherals.GPIOB.IDR.read().@"IDR[2]"));
+        if (SW3.state == 1) {
+            peripherals.GPIOC.ODR.modify(.{ .@"ODR[6]" = .High });
+        } else {
+            peripherals.GPIOC.ODR.modify(.{ .@"ODR[6]" = .Low });
+        }
+        if (SW3.state == 1 and last_state == 0) {
+            TestSR.startShift(&.{ id, 0x7F });
+            // Wrapping addition
+            id +%= 1;
+        }
+        last_state = SW3.state;
     }
 }
---
+++ src/subsystems/matrix.zig
@@ -0,0 +1,113 @@
+const microzig = @import("microzig");
+const std = @import("std");
+const peripherals = microzig.chip.peripherals;
+const periph_types = microzig.chip.types.peripherals;
+const RCC = peripherals.RCC;
+const DMA2 = peripherals.DMA2;
+const SPI1 = peripherals.SPI1;
+const GPIOB = peripherals.GPIOB;
+
+/// A struct representing an SPI interface to a chain of 8bit shift registers
+/// uses SPI1, pins B13 & B15
+///     num_srs: the number of 8 bit shift registers in the chain
+///     sysclock_divisor: the spi clock is sysclock / 2^(1+divisor)
+pub fn SrChain(num_srs: comptime_int, sysclock_divisor: u3) type {
+    return struct {
+        const DMA2_CH4: *volatile periph_types.bdma_v2.CH = getDmaCh(DMA2, 4);
+
+        /// Enables & condigures GPIOB
+        /// Enables & configures SPI1
+        /// Configures, but does not enable DMA2
+        pub fn setup() void {
+            // Enable clocks
+            RCC.AHBENR.modify(.{
+                .DMA2EN = 1,
+                .GPIOBEN = 1,
+            });
+            RCC.APB2ENR.modify(.{
+                .SPI1EN = 1,
+            });
+
+            // Setup GPIOB
+            GPIOB.MODER.modify(.{
+                .@"MODER[3]" = .Alternate,
+                .@"MODER[5]" = .Alternate,
+            });
+            GPIOB.AFR[1].modify(.{
+                .@"AFR[3]" = 0,
+                .@"AFR[5]" = 0,
+            });
+
+            // Setup SPI1
+            SPI1.CR1.modify(.{
+                .SPE = 0,
+            });
+            SPI1.CR1.modify(.{
+                .CPHA = .FirstEdge,
+                .CPOL = .IdleLow,
+                .MSTR = .Master,
+                .BR = @as(periph_types.spi_v2.BR, @enumFromInt(sysclock_divisor)),
+                .LSBFIRST = .MSBFirst,
+            });
+            SPI1.CR2.modify(.{
+                .TXDMAEN = 1,
+                .SSOE = 1,
+                .NSSP = 1,
+                .DS = .Bits8,
+            });
+            const cr1ptr: *volatile u32 = @ptrCast(&SPI1.CR1);
+            cr1ptr.* |= 64;
+
+            // Setup DMA
+            // Configure to respond to SPI requests only
+            // See Table 33.
+            // NOTE: MICROZIG IS BUGGED!! IT HAS A CS[0] FIELD, SO EVERYTHING IS OFF BY 1 HERE
+            // SO EVEN THO WE ARE USING CHANNEL 4, WE GOTTA USE FIELD CS[3]
+            DMA2.CSELR.modify(.{
+                .@"CS[3]" = 0b0011,
+            });
+
+            DMA2_CH4.CR.modify(.{
+                // Do not enable yet
+                .EN = 0,
+                .PSIZE = .Bits8,
+                .MSIZE = .Bits8,
+                .PL = .VeryHigh,
+                .MINC = 1,
+                .CIRC = 0,
+                .DIR = .FromMemory,
+            });
+
+            DMA2_CH4.PAR = @intFromPtr(&SPI1.DR16);
+            DMA2_CH4.NDTR.modify(.{
+                .NDT = 0,
+            });
+        }
+
+        /// Begins an async shift opperation.
+        /// Data must remain a valid pointer for the durration of the shift, as DMA will read from it
+        pub fn startShift(data: *const [num_srs]u8) void {
+            DMA2_CH4.CR.modify(.{
+                .EN = 0,
+            });
+            DMA2_CH4.MAR = @intFromPtr(data);
+            DMA2_CH4.NDTR.modify(.{
+                .NDT = num_srs,
+            });
+            DMA2_CH4.CR.modify(.{
+                .EN = 1,
+            });
+        }
+
+        pub fn sendSync(data: *const [num_srs]u8) void {
+            for (data) |byte| {
+                while (SPI1.SR.read().TXE == 0) {}
+                @as(*volatile u8, @ptrCast(&SPI1.DR16)).* = byte;
+            }
+        }
+    };
+}
+
+fn getDmaCh(dma: *volatile periph_types.bdma_v2.DMA, channel: comptime_int) *periph_types.bdma_v2.CH {
+    return @ptrFromInt(@intFromPtr(&dma.CH) + 20 * (channel - 1));
+}
