1f0fbf4b81294abb97f085eaaab777b428a0fefe
MicahSamuel77 <m.samuel0922@gmail.com>
2025-04-24 23:03:16 -0400

    feat: added button_a/b, as well as memory bytes

--- src/init/debounce.zig
+++ src/init/debounce.zig
@@ -1,18 +1,39 @@
-// const microzig = @import("microzig");
-// const RCC = microzig.chip.peripherals.RCC;
-// const FLASH = microzig.chip.peripherals.FLASH;
+const microzig = @import("microzig");
+const cImport = @import("../cImport.zig");
+const Joystick = @import("../subsystems/joystick.zig");
+const Button_A = @import("../subsystems/button_a.zig");
+const Button_B = @import("../subsystems/button_b.zig");
+const Screen = @import("../subsystems/Screen.zig");
+const cmsis = cImport.cmsis;
+const peripherals = microzig.chip.peripherals;
+const periph_types = microzig.chip.types.peripherals;
+const RCC = peripherals.RCC;
+const TIM14 = peripherals.TIM14;
+const GPIOC = peripherals.GPIOC;
 
-// void init_debounce_timer() {
+pub export fn TIM14_IRQHandler() callconv(.C) void {
+    TIM14.SR.modify(.{
+        .UIF = 0,
+    });
 
-//     RCC->APB1ENR |= RCC_APB1ENR_TIM14EN;
-//     TIM14->PSC = 24000 - 1;
-//     TIM14->ARR = 2 - 1;
-//     TIM14->DIER |= TIM_DIER_UIE;
-//     NVIC_EnableIRQ(TIM14_IRQn);
-//     TIM14->CR1 |= TIM_CR1_CEN;
+    // button_a memory byte
+    if (cImport.cmsis.GPIOC.*.IDR & cImport.cmsis.GPIO_IDR_4) {
+        Button_A.memory_byte = (Button_A.memory_byte << 1) + 1;
+    } else {
+        Button_A.memory_byte = (Button_A.memory_byte << 1);
+    }
 
-// }
+    // button_b memory byte
+    if (cImport.cmsis.GPIOC.*.IDR & cImport.cmsis.GPIO_IDR_3) {
+        Button_B.memory_byte = (Button_B.memory_byte << 1) + 1;
+    } else {
+        Button_B.memory_byte = (Button_B.memory_byte << 1);
+    }
 
-// void TIM14_IRQHandler() {
-//     TIM14-> SR &= ~TIM_SR_UIF;
-// }
+    // joystick memory byte
+    if (cImport.cmsis.GPIOC.*.IDR & cImport.cmsis.GPIO_IDR_4) {
+        Joystick.memory_byte = (Joystick.memory_byte << 1) + 1;
+    } else {
+        Joystick.memory_byte = (Joystick.memory_byte << 1);
+    }
+}
--- src/subsystems/button_a.zig
+++ src/subsystems/button_a.zig
@@ -5,12 +5,13 @@ const apps = @import("../main.zig").apps;
 
 var prev_reg_button_pressed = false;
 var cur_reg_button_pressed = false;
+pub var memory_byte: u8 = 0;
 
-pub fn button_update() void {
+pub fn update() void {
     prev_reg_button_pressed = cur_reg_button_pressed;
     cur_reg_button_pressed = (cImport.cmsis.GPIOC.*.IDR & cImport.cmsis.GPIO_IDR_4) != 0;
 }
 
-pub fn button_pressed() bool {
+pub fn pressed() bool {
     return (cur_reg_button_pressed and !prev_reg_button_pressed);
 }
