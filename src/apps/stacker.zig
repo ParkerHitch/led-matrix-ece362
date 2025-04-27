0ca963efd2891c4a77fdddbd504bc81ac0914d4d
Yericky <richye2005@gmail.com>
2025-04-27 03:07:45 -0400

    + difficulty cuz john

--- src/apps/stacker.zig
+++ src/apps/stacker.zig
@@ -88,7 +88,7 @@ fn appMain() callconv(.C) void {
     dt.start();
 
     // time keeping vairiables to limit tickRate
-    const tickRate: u32 = 30; // i.e. target fps or update rate
+    const tickRate: u32 = 50; // i.e. target fps or update rate
     const updateTime: u32 = 1000 / tickRate; // 1000 ms * (period of a tick)
     var timeSinceUpdate: u32 = 0;
 
@@ -238,7 +238,7 @@ fn appMain() callconv(.C) void {
             // only change / show top stack if haven't won or lost
             if (!win and !lose) {
                 // movment update
-                if ((@rem(tickcount, (8 - stack.height)) == 0) and !placed) { //(8 - stack.height)
+                if ((@rem(tickcount, (1 + 2 * (8 - stack.height)) == 0)) and !placed) { //(8 - stack.height)
                     stack.LayerArray[@intCast(stack.height)].x += xVel;
                     stack.LayerArray[@intCast(stack.height)].y += yVel;
                 }
