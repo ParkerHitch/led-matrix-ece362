def35a36e83f85219a8acf54fd5a135af4182455
Yericky <richye2005@gmail.com>
2025-04-27 02:48:37 -0400

    fixing stacker bugs 2 electric boogaloo

--- src/apps/stacker.zig
+++ src/apps/stacker.zig
@@ -191,8 +191,8 @@ fn appMain() callconv(.C) void {
 
                             // if x lower bound is lower than the stack, replace it
                             if (!lowerX) {
-                                stack.LayerArray[@intCast(stack.height)].x = stack.LayerArray[@intCast(stack.height - 1)].x;
                                 stack.LayerArray[@intCast(stack.height)].xLen = stack.LayerArray[@intCast(stack.height)].x + stack.LayerArray[@intCast(stack.height)].xLen - stack.LayerArray[@intCast(stack.height - 1)].x;
+                                stack.LayerArray[@intCast(stack.height)].x = stack.LayerArray[@intCast(stack.height - 1)].x;
                             }
                             // if x upper bound is higher than the stack, replace it
                             if (!upperX) {
@@ -200,8 +200,8 @@ fn appMain() callconv(.C) void {
                             }
                             // if y lower bound is lower than the bound, replace it
                             if (!lowerY) {
-                                stack.LayerArray[@intCast(stack.height)].y = stack.LayerArray[@intCast(stack.height - 1)].y;
                                 stack.LayerArray[@intCast(stack.height)].yLen = stack.LayerArray[@intCast(stack.height)].y + stack.LayerArray[@intCast(stack.height)].yLen - stack.LayerArray[@intCast(stack.height - 1)].y;
+                                stack.LayerArray[@intCast(stack.height)].y = stack.LayerArray[@intCast(stack.height - 1)].y;
                             }
                             // if y upper bound is lower than the bound, replace it
                             if (!upperY) {
